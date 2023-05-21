4adb842437f366b48e3344bb3a98b9f9764a8bd5
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-05-21 20:13:32 +0200

    one way data banding / directuva *ngIF

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
 
+  public name: string = 'iroman';
+  public age:number = 45;
+
+  get capitalizedName():string {
+    return  this.name.toLocaleUpperCase();
+  }
+
+  getHeroDescripcion():string{
+    return `${this.name} - ${this.age}`
+  }
+
+  changeHero():void{
+    this.name = 'batman';
+  }
+
+  changeAge(){
+    this.age = 50;
+  }
+
+  resetForm():void{
+  this.name= 'iroman';
+  this.age = 45;
+
+  document.querySelectorAll('h1')!.forEach(element =>{
+    element.innerHTML='<h1>Desde Angular</h1>';
+  })
+  }
 }
